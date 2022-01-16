2d3098f025c25630a092b8a57cac64d7c112ebbf
Abdellah DAMRI <damriabdellah@gmail.com>
2022-01-16 13:49:42 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
 
 // connect db
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
 // init middleware
 app.use(express.json({ extended: false }));
 
@@ -16,6 +14,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
